e7697552a72ba51a6f8a7fca279e4b9911a54a06
krantzinator <krantzrs8@gmail.com>
2017-03-07 21:19:18 -0500

    finishing up chapter 8 - coffeerun base modules

--- coffeerun/scripts/datastore.js
+++ coffeerun/scripts/datastore.js
@@ -1,4 +1,5 @@
 (function (window) {
+  'use strict';
   var App = window.App || {};
 
   function DataStore() {
--- coffeerun/scripts/truck.js
+++ coffeerun/scripts/truck.js
@@ -1,4 +1,5 @@
 (function (window) {
+  'use strict';
   var App = window.App || {};
 
   function Truck(truckId, db) {
@@ -14,7 +15,15 @@
   Truck.prototype.deliverOrder = function (customerId) {
     console.log('Delivering order for ' + customerId);
     this.db.remove(customerId);
-  }
+  };
+
+  Truck.prototype.printOrders = function () {
+    var customerIdArray = Object.keys(this.db.getAll());
+    console.log('Truck #' + this.truckId + ' has pending orders:');
+    customerIdArray.forEach(function (id) {
+      console.log(this.db.get(id));
+    }.bind(this));
+  };
 
   App.Truck = Truck;
   window.App = App;
