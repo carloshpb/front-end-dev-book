352854ec9bc05fb94c11f1cbcd6bf40cc0a568ab
krantzinator <krantzrs8@gmail.com>
2017-05-27 18:51:42 -0400

    ch 24 work

--- tracker/app/routes/sightings.js
+++ tracker/app/routes/sightings.js
@@ -2,21 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(){
-    let record1 = this.store.createRecord('sighting', {
-      location: 'Atlanta',
-      sightedAt: new Date('2016-02-09')
-    });
-
-    let record2 = this.store.createRecord('sighting', {
-      location: 'Calloway',
-      sightedAt: new Date('2016-03-14')
-    });
-
-    let record3 = this.store.createRecord('sighting', {
-      location: 'Asilomar',
-      sightedAt: new Date('2016-03-21')
-    });
-
-    return [record1, record2, record3];
+    return this.store.findAll('sighting', {reload: true});
   }
 });
